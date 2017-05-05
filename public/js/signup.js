dae3333d9df8e9c58bf4fe5adcb514665b714c42
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2017-05-04 20:34:28 -0400

    login working

--- public/js/signup.js
+++ public/js/signup.js
@@ -41,71 +41,9 @@ $('.tab a').on('click', function (e) {
  
 });
 
-$('#submitone').on('click', function () {
- console.log("Entry");
- window.location.href = 'index.html';
+// $('#submitone').on('click', function () {
+//  console.log("Entry");
+//  window.location.href = 'login.html';
 
-});
+// });
 
-$('#login').on('click', function(){
-   window.location.href = 'login.html';
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// module.exports = function validate() {
-      
-//    if( document.myForm.Name.value == "" )
-//    {
-//       alert( "Please provide your first name!" );
-//       document.myForm.first_name.focus() ;
-//       return false;
-//    }
-//     if( document.myForm.last_name.value == "" )
-//    {
-//       alert( "Please provide your last name!" );
-//       document.myForm.last_name.focus() ;
-//       return false;
-//    }
-
-//    if( document.myForm.user_name.value == "" )
-//    {
-//       alert( "Please provide a user name!" );
-//       document.myForm.user_name.focus() ;
-//       return false;
-//    }
-//     if( document.myForm.email_address.value == "" )
-//    {
-//       alert( "Please provide a Email Address!" );
-//       document.myForm.email_address.focus() ;
-//       return false;
-//    }
-
-//      if( document.myForm.password.value == "" )
-//    {
-//       alert( "Please provide a password!" );
-//       document.myForm.password.focus() ;
-//       return false;
-//    }
-// }//end function
-
-// };
